68c3fbdc529aa93c858af7861cf4d2bb88cf2936
lexy5251 <xixi5251@gmail.com>
2020-09-30 19:45:28 -0400

    Add app state change listener

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -9,24 +9,43 @@ import ResultScreen from './screens/ResultScreen';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import { AppState } from "react-native";
 
-const Stack = createStackNavigator();
+const App = () => {
+  useEffect(() => {
+    AppState.addEventListener('change', handleChange);  
+  
+    return () => {
+      AppState.removeEventListener('change', handleChange);  
+    }
+  }, []);
+  
+  
+  const handleChange = (newState) => {
+    if (newState === "active") {
+      console.log(">>>>>hahaha");
+    }
+  }
+  const Stack = createStackNavigator();
 
-export default function App() {
+  return (<NavigationContainer>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Practice" component={PracticeScreen} />
+      <Stack.Screen name="Test" component={TestScreen} />
+      <Stack.Screen name="Result" component={ResultScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>)
+}
+
+export default function AppWrapper() {
   const store = createStore(rootReducer); 
   store.subscribe(() => {
     console.log('store is: ', store.getState());
   })
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Practice" component={PracticeScreen} />
-          <Stack.Screen name="Test" component={TestScreen} />
-          <Stack.Screen name="Result" component={ResultScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <App />
     </Provider>
   );
 }

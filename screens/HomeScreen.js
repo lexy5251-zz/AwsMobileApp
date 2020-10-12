8658802210f9e7616cd9f0251b806210bab7ef13
lexy5251 <xixi5251@gmail.com>
2020-10-12 16:16:27 -0400

    multi choices and progress in home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,51 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 import { createQuestions } from '../test/TestData';
 import _ from 'lodash'
 import { startCurrentPractice, startCurrentTest } from '../actions';
-import {pickQuestionsRandomly} from '../data'
+import {getData} from '../data'
+import { useFocusEffect } from '@react-navigation/native';
+
 
 export default function HomeScreen({ navigation }) {
   const dispatch = useDispatch();
   const currentPractice = useSelector(state => state.currentPractice);
   const currentTest = useSelector(state => state.currentTest);
+  const [progress, setProgress] = useState({total: 1185});
   console.log(">>>>>>>home screen current practice", currentPractice, (!currentPractice && true));
 
   console.log(">>>>>>>home screen current test", currentTest, (!currentTest && true));
 
+  useFocusEffect(
+    React.useCallback(() => {
+      console.log('>>>>>focus!!');
+      getProgress('c01').then(p => setProgress(p));
+      return () => {};
+    }, [])
+  );
+
+  const getProgress = (examVersion) => {
+    let key = `@${examVersion}_progress`;
+    return getData(key).then((v) => {
+      console.log(">>>>>>v", v);
+
+      let progress = {total: 1185, learned: 0, mistakes: 0};
+      if (!v) {
+        return progress;
+      }
+      let wrongNum = Object.values(v).filter(i => i === 'wrong').length;
+      let correctNum = Object.values(v).filter(i => i === 'correct').length;
+      progress.learned = correctNum;
+      progress.mistakes = wrongNum;
+      return progress;
+    });
+  }
+  
   return (
     <View style={styles.container}>
     <Text>Practice</Text>
+    {!_.isEmpty(progress) && <Text>Total: {progress.total}, learned: {progress.learned}, mistakes: {progress.mistakes}</Text>}
     <View style={styles.buttonContainer}>
       <TouchableOpacity
         style = {styles.button}
